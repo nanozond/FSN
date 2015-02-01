08f9af25e51347c1b447558b3557f07cb5926b36
nanozond <nanozond@gmail.com>
2015-02-01 23:55:40 +0200

    - bug fix

--- src/main/java/org/ts_labs/example/Main.java
+++ src/main/java/org/ts_labs/example/Main.java
@@ -11,6 +11,7 @@ public class Main {
     public static void main(String[] args) {
         ConsolePrinter.print(Localization.Messages.HELLO);
         FileSystemNavigator fsn = new FileSystemNavigator();
+
         fsn.waitForInput();
     }
 }
